7d411cc8fc66a0218eb51237acca029f7d28c877
wangxiang <1827945911@qq.com>
2021-11-20 22:34:12 +0800

    添加eq工具

--- func/eq.d.ts
+++ func/eq.d.ts
@@ -1,10 +1,11 @@
 /**
- * 比较两个值，以确定它们是否相等
+ * 执行一个[`相同的值为零`](http://ecma-international.org/ecma-262/6.0/#sec-samevaluezero)
+ * 比较两个值,以确定它们是否相等
  *
  * @category Lang
  * @param {*} value 要比较的值
  * @param {*} other 要比较的另一个值
- * @returns {boolean} 如果值相等则返回' true '，否则返回' false '
+ * @returns {boolean} 如果值相等则返回 'true' 否则返回 'false'
  */
 export declare function eq(value: any, other: any): boolean
 
--- func/eq.js
+++ func/eq.js
@@ -6,16 +6,16 @@
  * @author: entfrm开发团队-王翔
  *
  * @create: 2021-11-18
- **/
+ */
 
 /**
- * 执行一个[`相同的值为零`](http://ecma-international.org/ecma-262/7.0/#sec-samevaluezero)
- * 比较两个值，以确定它们是否相等
+ * 执行一个[相同的值为零](http://ecma-international.org/ecma-262/7.0/#sec-samevaluezero)
+ * 比较两个值,以确定它们是否相等
  *
  * @category Lang
  * @param {*} value 要比较的值
  * @param {*} other 要比较的另一个值
- * @returns {boolean} 如果值相等则返回' true '，否则返回' false '
+ * @returns {boolean} 如果值相等则返回 'true' 否则返回 'false'
  * @example
  *
  * const object = { 'a': 1 }
