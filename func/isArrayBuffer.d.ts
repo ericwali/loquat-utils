276579a6962730d7696d2e696f824b61c16274d1
wangxiang <1827945911@qq.com>
2021-11-21 10:22:43 +0800

    添加isArrayBuffer工具

--- func/isArrayBuffer.d.ts
+++ func/isArrayBuffer.d.ts
@@ -5,7 +5,7 @@
  * @param {*} value 要检查的值
  * @returns {boolean} 如果 'value' 是一个数组缓冲区,则返回 'true' 否则返回 'false'
  */
-export declare function isArrayBuffer(value: any): value is ArrayBuffer
+export declare function isArrayBuffer(value?: any): value is ArrayBuffer
 
 declare module './ctor' {
   interface LoquatMethods {
